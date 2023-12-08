c0e0a70cd9079d1f5652c5cace5d21efc2ca3c23
Nsirival <55929427+Nsirival@users.noreply.github.com>
2023-12-08 09:52:04 -0800

    Update linkedListFuncs.cpp

--- linkedListFuncs.cpp
+++ linkedListFuncs.cpp
@@ -12,7 +12,11 @@ using namespace std;
 //return sum of all values in linked list using a recursive approach
 //if head is null return 0
 int recursiveSum(Node* head) {
-  return -42;
+  if(head -> next == nullptr){
+    return head -> data;
+  } else {
+    return head -> data + recursiveSum(head->next);
+  }
 }
 
 
@@ -20,8 +24,10 @@ int recursiveSum(Node* head) {
 //return the largest value in the linked list using a recursive approach
 //you may assume the list has at least one element
 int recursiveLargestValue(Node* head) {
-
-  return -42;
+  if(head -> next == nullptr){
+     if()
+  } 
+  
 }
 
 
